67ed9fd02f003e6f46c40a4976731ad9085d9286
SohamMangale <sohammangale60@gmail.com>
2024-10-11 10:11:39 +0530

    Create Exp.12B.cpp

---
+++ Exp.12B.cpp
@@ -0,0 +1,37 @@
+//soham
+//23070123084
+//entc-B1
+//experiment-12A
+#include <iostream>
+using namespace std;
+//defining the constructor outside the class
+class student
+{
+    int rn;
+    char name[50];
+    float fee;
+    public:
+    student();
+    void display();
+};
+student::student()
+{
+    cout<<"Enter the name: ";
+    cin>>name;
+    cout<<"Enter the roll no.: ";
+    cin>>rn;
+    cout<<"Enter the fee: ";
+    cin>>fee;
+}
+void student::display()
+{
+    cout<<endl;
+    cout<<"Name: "<<name<<endl;
+    cout<<"Roll No: "<<rn<<endl;
+    cout<<"Fee: "<<fee<<endl;
+}
+int main()
+{
+    student s1;
+    s1.display();
+}
